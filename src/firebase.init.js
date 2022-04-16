9c735bc705b7869dfd597057cbbe093398835887
nagimuddin <97009292+nagimuddin@users.noreply.github.com>
2022-04-17 01:00:22 +0600

    created environment varriable

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -6,12 +6,12 @@ import {getAuth} from 'firebase/auth';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBgOhj0v2w2v06DREfJe-2usDYDuQ0UOTM",
-  authDomain: "wedding-photographer-5943c.firebaseapp.com",
-  projectId: "wedding-photographer-5943c",
-  storageBucket: "wedding-photographer-5943c.appspot.com",
-  messagingSenderId: "478757959172",
-  appId: "1:478757959172:web:beb60a88f088bf3b292751"
+apiKey: process.env.REACT_APP_apiKey,
+authDomain: process.env.REACT_APP_authDomain,
+projectId: process.env.REACT_APP_projectId,
+storageBucket: process.env.REACT_APP_storageBucket,
+messagingSenderId: process.env.REACT_APP_messagingSenderId,
+appId: process.env.REACT_APP_appId,
 };
 
 // Initialize Firebase
