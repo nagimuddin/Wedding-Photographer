287c41b7f4e040d2f15800194e0a38856f50b5f4
nagimuddin <97009292+nagimuddin@users.noreply.github.com>
2022-04-18 00:30:13 +0600

    bennar section update

--- src/Pages/Shared/Bennar/Bennar.js
+++ src/Pages/Shared/Bennar/Bennar.js
@@ -1,10 +1,49 @@
 import React from 'react';
+import './Bennar.css'
+import bennar1 from '../../../images/bennar/bennar1.jpg';
+import bennar2 from '../../../images/bennar/bennar2.jpg';
+import bennar3 from '../../../images/bennar/bennar3.jpg';
+import { Carousel } from 'react-bootstrap';
 
 const Bennar = () => {
     return (
-        <div>
-            <h2>Bennar</h2>
-        </div>
+        <Carousel fade>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src={bennar1}
+            alt="First slide"
+          />
+          <Carousel.Caption>
+            <h3>Hannah</h3>
+            <p>Beautiful Wedding Quotes to Celebrate Love and Partnership.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src={bennar2}
+            alt="Second slide"
+          />
+      
+          <Carousel.Caption>
+            <h3>Engage Time</h3>
+            <p>Beautiful Wedding Quotes to Celebrate Love and Partnership.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src={bennar3}
+            alt="Third slide"
+          />
+      
+          <Carousel.Caption>
+            <h3>Happy Hour</h3>
+            <p>Beautiful Wedding Quotes to Celebrate Love and Partnership.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
     );
 };
 
--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -14,7 +14,7 @@ const Header = () => {
   <Navbar.Toggle aria-controls="responsive-navbar-nav" />
   <Navbar.Collapse id="responsive-navbar-nav">
     <Nav className="ms-auto">
-      <Nav.Link as={Link} to="/home">Home</Nav.Link>
+      <Nav.Link as={Link} to="/">Home</Nav.Link>
       <Nav.Link as={Link} to="/service">Sevice</Nav.Link>
       <Nav.Link as={Link} to="/login">Login</Nav.Link>
       <Nav.Link as={Link} to="/blogs">Blogs</Nav.Link>
